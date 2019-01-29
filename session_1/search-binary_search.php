01875aceffe2319c9c6f31cfe1112d890bb9117e
yaremkoPavlo <yaremko.pavlo@gmail.com>
2019-01-29 12:17:25 +0200

    add recursive

--- session_1/search-binary_search.php
+++ session_1/search-binary_search.php
@@ -21,10 +21,27 @@ function binarySearch($el, $arr) {
 }
 
 function mid($start, $end) {
-	return floor(($start + $end) / 2);
+	return floor($start + ($end - $start) / 2);
 }
 
 $arr = [2, 6, 13, 21, 36, 47, 63, 81, 97];
 
-echo binarySearch(82, $arr);
+//echo binarySearch(82, $arr);
+
+# recursion binary search
+
+function binarySearchR($el, $arr, $start, $end) {
+	$mid = mid($start, $end);
+	if ($el == $arr[$mid])
+		return $mid;
+	if ($start >= $end) 
+		return -1;
+	if ($el > $arr[$mid]) {
+		return binarySearchR($el, $arr, $mid+1, $end);
+	} else {
+		return binarySearchR($el, $arr, $start, $mid-1);
+	}
+}
+
+echo binarySearchR(2, $arr, 0, 8);
 ?>
